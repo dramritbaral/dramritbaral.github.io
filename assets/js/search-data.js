efebe5a43ca77de1a9abd5e1a1e56c78626a1837
dramritbaral <dr.amritbaral@gmail.com>
2025-05-01 20:21:57 +0000

    Deploying to gh-pages from @ dramritbaral/dramritbaral.github.io@6bbcf4c6ddc9d6d33eed7c3728e31ea4310f7738 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -147,14 +147,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%72.%61%6D%72%69%74%62%61%72%61%6C@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/amritbaral", "_blank");
         },
       },{
         id: 'social-rss',
@@ -168,14 +168,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=KIZXupIAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
